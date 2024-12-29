f350976012f61d64be2c2227b51f08718c11683d
obolleurs <obolleurs@gmail.com>
2024-12-29 16:10:58 +0100

    First draft of MLE NOT WORKING YET

--- main.m
+++ main.m
@@ -4,13 +4,14 @@ clc
 load("data.mat")
 
 % Amount of iterations
-K = 5000;
+%K = 5000;
 
 % Plot formation and error
 plot_formation(z, "Initial state")
 
 % Position vector in 2D in time per drone/agent
 z_pos = zeros(K,N,2);
+
 z_pos(1,:,:) = z;
 z = reshape(z_pos(1,:,:), size(z));
 
@@ -24,7 +25,7 @@ pos_err = zeros(K,1);
 % Array with the trajectory of one node in 2D
 trajectory = zeros(K,N,2);
 
-%% Formation control noiseless case
+% Formation control noiseless case
 for k = 1:K
     for i = 4:N
         % Reshape z_pos per node a 2D matrix
@@ -53,8 +54,24 @@ disp("Final error noiseless case")
 disp(pos_err(end))
 
 %% Formation control Noise case
+clear all 
+
 % Initialization
+load('data.mat')
+% Position vector in 2D in time per drone/agent
+z_pos = zeros(K,N,2);
+
+z_pos(1,:,:) = z;
+z = reshape(z_pos(1,:,:), size(z));
+
+% Initialization
+U = zeros(K,N,2);
+dist = zeros(K,N,2);
+
+% Positional error from the optimum location
+pos_err = zeros(K,1);
 noise_power = 5;
+
 for k = 1:K
     for i = 4:N
         % Generate noise
@@ -86,8 +103,25 @@ disp("Final error noise case")
 disp(pos_err(end))
 
 %% Formation control Noise case with averaging over 10 samples estimator
+clear all 
+
 % Initialization
+load('data.mat')
+% Position vector in 2D in time per drone/agent
+z_pos = zeros(K,N,2);
+
+z_pos(1,:,:) = z;
+z = reshape(z_pos(1,:,:), size(z));
+
+% Initialization
+U = zeros(K,N,2);
+dist = zeros(K,N,2);
+
+% Positional error from the optimum location
+pos_err = zeros(K,1);
 MA_size = 10;
+noise_power = 5;
+
 for k = 1:K
     for i = 4:N
         % Generate noise
@@ -111,7 +145,6 @@ for k = 1:K
 
         % Change position according to input
         z_pos(k+1,i,:) = z_pos(k,i,:) + 10*dt*U(k,i,:);
-
         % Reshape 2D z_pos per node to fill into z with all nodes
         z(i,:) = reshape(z_pos(k+1,i,:), size(z(i,:)));
     end
@@ -126,4 +159,68 @@ yscale("log")
 title("Error noise case with first estimator")
 
 disp("Final error noise case")
+disp(pos_err(end))
+
+%% Formation control Noise case with MLE due to Gaussian noise and linear model estimator
+
+clear all 
+
+% Initialization
+load('data.mat')
+% Position vector in 2D in time per drone/agent
+%z_pos = zeros(K,N,2);
+
+%z_pos(1,:,:) = z;
+%z = reshape(z_pos(1,:,:), size(z));
+
+% Initialization
+%U = zeros(K,N,2);
+%dist = zeros(K,N,2);
+
+% Positional error from the optimum location
+pos_err = zeros(K,1);
+noise_power = 5;
+T = 100;
+H = kron(ones(N),eye(2));
+Rtilde = kron(ones(N),R);
+for k = 1:K
+    for i = 4:N
+        % Generate noise
+        v = noise_power*randn(14)*Rtilde;
+        %CovMatrix = cov(v);
+        %invCovMatrix = inv(CovMatrix);
+
+        % Reshape z_pos per node a 2D matrix
+        %z_i = reshape(z_pos(k,i,:), size(z(i,:)));
+
+        % Calculate the current distance
+        distance = z(i,:)-z+v;
+
+        %calculate thetaHat using the BLUE formula
+        %thetaHat = inv(z * invCovMatrix * z')*z * invCovMatrix * z_pos(k,i,:)';
+
+        %MLE formulation
+        y = H*distance;
+        zHat = (1/T).*H'*y;
+        % Caluclate the current input
+        %U(k,i,:) = L(i,:)*zHat;
+        U = L*zHat;
+        % Change position according to input
+        %z_pos(k+1,i,:) = z_pos(k,i,:) + 10*dt*U(k,i,:);
+        z = z + dt*U ;
+
+        % Reshape 2D z_pos per node to fill into z with all nodes
+        %z(i,:) = reshape(z_pos(k+1,i,:), size(z(i,:)));
+    end
+    pos_err(k) = norm(z-z_star,2);
+    %trajectory(k,:,:) = z_pos(k,:,:);
+end
+plot_formation(z, "Final state (?? iterations) noise case with MLE estimator");
+
+figure
+plot(pos_err)
+yscale("log")
+title("Error noise case with MLE estimator")
+
+disp("Final error MLE noise case")
 disp(pos_err(end))
